bfda1cb4fda757f3c9fc67f943b4aebfcdb218c7
Junaid Farooq <ijunaidfarooq@gmail.com>
2015-10-14 21:36:44 +0500

    Added CRUD Ops to App

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -4,6 +4,7 @@ class PollsController < ApplicationController
 		@poll = Poll.new	
 		@polls = Poll.all
 	end
+
 	def create
 		@poll = Poll.new poll_params
 		if @poll.save
@@ -12,16 +13,30 @@ class PollsController < ApplicationController
 		end
 	end
 
-	def show
-		@polls = Poll.all
+	def edit
+		@poll = Poll.find(params[:id])
 	end
 
+	def show
+		@poll = Poll.find(params[:id])
 
+	end
+
+	def update
+		@poll = Poll.find(params[:id])
+		if @poll.update_attributes poll_params
+			redirect_to "/newpoll"
+		end
+	end
+
+	def destroy
+		Poll.find(params[:id]).destroy
+		redirect_to "/newpoll"
+	end
 private
 
 def poll_params
 	params.require(:poll).permit(:title)
 end
-
 
 end
